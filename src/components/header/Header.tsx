b632707dbd6e60cb7e96b739260c8a594cba70bd
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-04 16:26:41 +0000

    fix folders names

--- src/components/header/Header.tsx
+++
@@ -1,5 +0,0 @@
-export const Header = () => {
-    return <></>
-}
-
-export default Header;
--- src/components/main/hero/heroHeading/HeroHeading.tsx
+++
@@ -1,7 +0,0 @@
-import { HHeading } from "@/components/main/hero/heroHeading/HeroHeading.style";
-
-export const HeroHeading = () => {
-  return <HHeading>opa</HHeading>;
-};
-
-export default HeroHeading;
