d4ca3dc6946b42487de3690f4fac2db8da9d6d83
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-04 22:38:19 +0000

    Update dependencies and add new ones
    Test if gitpod is connected to the github repo

--- src/components/Header/HeaderDropdown/HeaderDropdown.tsx
+++ src/components/Header/HeaderDropdown/HeaderDropdown.tsx
@@ -1,33 +1,36 @@
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import React from 'react';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+
 import Link from "next/link";
+ import {DContent, DItem} from "@/components/Header/HeaderDropdown/HeaderDropdown.style";
 import {HamburgerMenuIcon} from "@radix-ui/react-icons";
-import {DContent, DItem} from "@/components/Header/HeaderDropdown/HeaderDropdown.style";
 
 function HeaderDropdown() {
     return (
         <DropdownMenu.Root>
-            <DropdownMenu.Trigger asChild>
-                <HamburgerMenuIcon/>
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Portal>
-                <DContent sideOffset={16}>
-                    <DItem>
-                        <Link href="/"> Home
-                        </Link>
-                    </DItem>
-                    <DItem>
-                        <Link href="/projetos"> Projetos
-                        </Link>
-                    </DItem>
-                    <DItem>
-                        <Link href="/#contato"> Contato
-                        </Link>
-                    </DItem>
-                </DContent>
-            </DropdownMenu.Portal>
-
-        </DropdownMenu.Root>)
+             <DropdownMenu.Trigger asChild>
+                 <HamburgerMenuIcon/>
+             </DropdownMenu.Trigger>
+        
+             <DropdownMenu.Portal>
+                 <DContent sideOffset={16}>
+                     <DItem>
+                         <Link href="/"> Home
+                         </Link>
+                     </DItem>
+                     <DItem>
+                         <Link href="/projetos"> Projetos
+                         </Link>
+                     </DItem>
+                     <DItem>
+                         <Link href="/#contato"> Contato
+                         </Link>
+                     </DItem>
+                 </DContent>
+        
+             </DropdownMenu.Portal>
+         </DropdownMenu.Root>
+    )
 }
 
 export default HeaderDropdown;
