e662103f9962ed09a6135072d897d0f9c82d4c5b
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-05 16:28:16 +0000

    Remove LinksList import from Hero component
    The Hero component's code has been revised to no longer import the LinksList module.

--- src/components/Main/hero/Hero.tsx
+++ src/components/Main/hero/Hero.tsx
@@ -1,6 +1,5 @@
 import { HeroHeading } from "./heroHeading";
-import LinksList from "@/components/Main/hero/LinksList";
-import { heroHeading, heroParagraph } from "@/services/data";
+import { heroHeading} from "@/services/data";
 import { Name } from "@/components/Main/hero/heroHeading/HeroHeading.style";
 import HeroInfo from "@/components/Main/hero/HeroInfo";
 
