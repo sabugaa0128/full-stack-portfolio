070bbd623be135e489dc3e51f0e34647c7f85cab
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-05 16:26:30 +0000

    Refactor HeroHeading component for dynamic content
    The HeroHeading component has been updated to render children props dynamically. Associated static content has been moved to the data service file.

--- src/components/Main/hero/heroHeading/HeroHeading.tsx
+++ src/components/Main/hero/heroHeading/HeroHeading.tsx
@@ -1,16 +1,8 @@
-import {HeroText, HHeading, Name} from "@/components/Main/hero/heroHeading/HeroHeading.style";
+import { HHeading } from "@/components/Main/hero/heroHeading/HeroHeading.style";
+import { ChildrenProps } from "@/types/children";
 
-export const HeroHeading = () => {
-  return (
-    <HHeading>
-      <Name>Bruno Moleta. Full-stack Developer.</Name>
-      <HeroText>
-        Sou um profissional multidisciplinar, versátil e dinâmico com graduação
-        em arquitetura, passagem pelo universo de startups e formação em
-        desenvolvimento full-stack.
-      </HeroText>
-    </HHeading>
-  );
+export const HeroHeading = ({ children }: ChildrenProps) => {
+  return <HHeading>{children}</HHeading>;
 };
 
 export default HeroHeading;
--- src/services/data.ts
+++ src/services/data.ts
@@ -1,7 +1,33 @@
-export const headerItems: string[] = [
-    "início",
-    "sobre",
-    "projetos",
-    "contato"
+export const headerItems: string[] = ["início", "sobre", "projetos", "contato"];
+
+export const heroHeading = "Bruno Moleta. Full-stack Developer.";
+export const heroParagraph =
+    "Sou um profissional multidisciplinar, versátil e dinâmico com graduação em arquitetura, passagem pelo universo de startups e formação em desenvolvimento full-stack.";
+
+export const linksList = [
+    {
+        name: 'github',
+        href: "https://github.com/brunomoleta"
+    },
+    {
+        name: 'linkedin',
+        href: "https://www.linkedin.com/in/bruno-moleta-santos-desenvolvedor-full-stack/"
+    },
+    {
+        name: 'currículo vitae',
+        href: '/'
+    }
 ]
 
+export const dropdownList = [{
+        name: 'home',
+        href: "/"
+    },
+    {
+        name: 'sobre',
+        href: "/sobre-o-bruno"
+    },
+    {
+        name: 'contato',
+        href: '/contato'
+    }]
