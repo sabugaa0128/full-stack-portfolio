fb1b0b0dbd3c81179bbdd744f0619c97c2f2421e
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-07 16:49:23 +0000

    Add ProjectsSection and update Stack components
    Added ProjectsSection to the Home function in page.tsx. The Stack components were also updated to integrate a new property 'stackType' and the corresponding techList this provides a more specific categorization for the tech stacks.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import { Hero } from "@/components/Main/hero";
 import DecorSection from "@/components/Main/DecorSection";
 import { Stack } from "@/components/Main/Stack";
 import { backEndList, frontEndList } from "@/services/data";
+import {ProjectsSection} from "@/components/Main/Projects";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <DecorSection />
-      <Stack techList={frontEndList} />
-      <Stack techList={backEndList} />
+        <Stack stackType="front end" techList={frontEndList} />
+        <Stack stackType="back end e outros" techList={backEndList} />
+        <ProjectsSection/>
     </>
   );
 }
