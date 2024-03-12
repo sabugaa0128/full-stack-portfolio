8285d2b6cde71244370d072bce495f84e014169d
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-12 18:46:47 -0300

    Refactor SingleProject component and related styles
    The SingleProject component is restructured to improve reusability and clarity. Furthermore, the SingleProject component now accepts children props for better customization.

--- src/components/SingleProject/SingleProject.tsx
+++ src/components/SingleProject/SingleProject.tsx
@@ -1,38 +1,21 @@
 import React from "react";
-import { projects } from "@/services/data";
-import { Name } from "@/components/Main/hero/heroHeading/HeroHeading.style";
-import SectionHeader from "@/components/SectionHeader";
 
-import {
-  About,
-  HeadInfoWrapper,
-  HeadWrapper,
-} from "@/components/SingleProject/SingleProject.style";
-import ProjectDescription from "@/components/SingleProject/ProjectDescription";
-import ProjectLinks from "@/components/SingleProject/ProjectLinks";
+import SectionHeader from "@/components/SectionHeader";
 import { ProjBtn } from "@/components/Main/Projects/projectCard/ProjectBtn/ProjectBtn.style";
 import { ArrowLeftIcon } from "@radix-ui/react-icons";
 
-function SingleProject({ projectNumber }: { projectNumber: number }) {
-  const { title, about1, about2 } = projects[projectNumber];
+import { ProjArticle } from "@/components/SingleProject/SingleProject.style";
+
+import { ChildrenProps } from "@/types/types";
+
+function SingleProject({ children }: ChildrenProps) {
   return (
     <>
-      <article>
+      <ProjArticle>
         <SectionHeader changeH="h3" word="projeto" />
-        <HeadInfoWrapper>
-          <Name style={{ borderBlockEnd: "4px solid var(--teal-30)" }}>
-            {title}
-          </Name>
-          <HeadWrapper>
-            <About style={{ color: "var(--cool-grey-60)" }} as="section">
-              <p>{about1}</p>
-              <p>{about2}</p>
-            </About>
-            <ProjectDescription project={projects[projectNumber]} />
-          </HeadWrapper>
-        </HeadInfoWrapper>
-      </article>
-      <ProjectLinks project={projects[projectNumber]} />
+        {children}
+      </ProjArticle>
+
       <ProjBtn href={"/#inicio"}>
         <ArrowLeftIcon />
         Voltar para página inicial
