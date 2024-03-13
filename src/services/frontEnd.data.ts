e1e6a76cbb7b6bbdde0b2e3446afae79803cc83b
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-13 17:04:14 -0300

    Update front-end data in Durval project
    update explanations of the project in hand

--- src/services/frontEnd.data.ts
+++ src/services/frontEnd.data.ts
@@ -7,8 +7,19 @@ import UpdateEB from "@/assets/Easy/Update-Multi.png";
 import LoginFormEB from "@/assets/Easy/Form-Login.png";
 import FormEB from "@/assets/Easy/MultiStepForm-Multi.png";
 
+import HomeD from "@/assets/Durval/Home-Multi.png";
+import CartD from "@/assets/Durval/Cart-Multi.png";
+import AddressesD from "@/assets/Durval/addresses-multi.png";
+import SignUpD from "@/assets/Durval/signup-multi.png";
+import CrudD from "@/assets/Durval/CRID.png";
+import ErrorMessagesD from "@/assets/Durval/Error-messages.png";
+import SingleD from "@/assets/Durval/singleProduct-multi.png";
+import SearchD from "@/assets/Durval/Search-Multi.png";
+import LoginD from "@/assets/Durval/Login.png";
+import DashD from "@/assets/Durval/dashboard-multi.png";
+
 import { IImage } from "@/types/types";
-import { altTemplateMulti, TwoMobiles } from "@/services/service";
+import {altTemplateMulti, threeMobiles, twoMobiles} from "@/services/service";
 
 export const EasyBankFront: IImage[] = [
   {
@@ -27,7 +38,7 @@ export const EasyBankFront: IImage[] = [
   },
   {
     image: UpdateEB,
-    alt: TwoMobiles("Dashboard", "edição de senha"),
+    alt: twoMobiles("Dashboard", "edição de senha"),
     small: "atualização de dados pessoais",
     explain:
       "Dentro da Dashboard ele pode ler e editar informações pessoais (ex: se assessor, mudar de especialidade) e\n" +
@@ -44,7 +55,7 @@ export const EasyBankFront: IImage[] = [
   },
   {
     image: LoginFormEB,
-    alt: TwoMobiles(
+    alt: twoMobiles(
       "tela de login para envio de email",
       "tela de login final que é o envio de senha",
     ),
@@ -70,10 +81,97 @@ export const EasyBankFront: IImage[] = [
 
 export const DurvalFront: IImage[] = [
   {
-    image: HomeEB,
-    alt: altTemplateMulti("a homepage"),
+    image: HomeD,
+    alt: altTemplateMulti("a página inicial"),
     small: "inicial",
     explain:
-      "O usuário pode conhecer a empresa, clicar em 'Entrar' para acessar a conta ou clicar 'Conhecer assessores'.",
+      "Além de navegar pela página o usuário pode colocar um produto no carrinho(que será salvo no Local Storage), fazer uma" +
+        " busca escrita e procurar por categoria ou marca do equipamento.",
+  },
+  {
+    image: SearchD,
+    alt: altTemplateMulti(
+      "a listagem da busca por palavra-chave",
+    ),
+    small: "busca por palavra-chave",
+    explain:
+      "Listagem dos produtos que possuem a palavra procurada pelo usuário dentro do seu nome.",
+  },
+  {
+    image: SingleD,
+    alt: altTemplateMulti("o detalhamento de um produto específico"),
+    small: "inicial",
+    explain:
+        "Ao clicar no Card de algum produto abre a página dele com informações detalhadas.",
+  },
+  {
+    image: CartD,
+    alt: altTemplateMulti("o Carrinho de compras em aberto"),
+    small: "carrinho de compras.",
+    explain:
+        "Ao clicar no botão 'adicionar no carrinho' o produto é... adicionado ao carrinho. " +
+        " Dentro dele é possível aumentar ou diminuir a quantidade de itens, remover um equipamento" +
+        " e partir para a finalização da compra." +
+        " Note que o componente que o envolve é um Modal, iniciado com Radix UI, o qual" +
+        " é usado em outras situações que serão mostradas a seguir."
+  },
+  {
+    image: SignUpD,
+    alt: altTemplateMulti(
+      "o cadastro do usuário dentro de um modal",
+    ),
+    small: "cadastro do usuário",
+    explain:
+      "Formulário separado em várias etapas. Foi feita uma concatenação de um objeto a " +
+        "cada vez que o usuário clica 'avançar' e na última etapa os dados são enviados" +
+        " ao banco de dados e o objeto é apagado.",
+  },
+  {
+    image: LoginD,
+    alt: threeMobiles("coloca o e-mail","requisitos mínimos para enviar os dados","envio feito." +
+        " Aguardando a resposta do banco de dados"),
+    small: "login",
+    explain:
+        "O login segue a mesma lógica de etapas separadas para o envio do formulário." +
+        " Na tela do meio, há um exemplo de resposta da validação de dados através da biblioteca Zod." +
+        " Na terceira tela os dados foram enviados" +
+        " e estamos aguardando a resposta do back end.",
+  },
+  {
+    image: DashD,
+    alt: altTemplateMulti("o dashboard do usuário"),
+    small: "dashboard do usuário logado",
+    explain:
+        "central das informações do usuário logado." +
+        " A partir daqui este pode ler, criar, editar e remover" +
+        " seus endereços, formas de pagamento e anúncios."
+  },
+  {
+    image: AddressesD,
+    alt: threeMobiles(
+        "criação","edição","remoção de endereço do usuário"
+    ),
+    small: "dashboard | endereços",
+    explain:
+        "Listagem dos endereços cadastrados com a possibilidade de criar, editar ou remover algum deles." +
+        " Ao clicar em 'editar' o endereço 'Savassi', por exemplo, abrirá um Modal."
+  },
+  {
+    image: CrudD,
+    alt: altTemplateMulti(
+        "operações de leitura, criação, edição e deleção do ende dashboard",
+    ),
+    small: "criar, editar e remover um endereço.",
+    explain:
+        " O Modal é o mesmo componente usado no Cart. Foi construído com flexibilidade.",
+  },
+  {
+    image: ErrorMessagesD,
+    alt: altTemplateMulti(
+      "operações de leitura, criação, edição e deleção do dashboard",
+    ),
+    small: "1ª: Carrinho vazio, 2ª: Busca por palavra-chave sem sucesso, 3ª: Usuário digita um endereço de url que não existe.",
+    explain:
+      "Páginas de tratamento de exceção.",
   },
 ];
