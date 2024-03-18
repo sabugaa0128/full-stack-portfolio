75f54360fdc15829da4a607b1b1a927f61ce79c9
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-18 17:16:53 -0300

    Add ProjParagraphs component and refactor ProjectDescription
    Introduced a new ProjParagraphs component to consolidate project information. Also refactored the ProjectDescription component for improved readability and to facilitate rendering based on project infoType.

--- src/components/SingleProject/ProjectDescription/ProjectDescription.tsx
+++ src/components/SingleProject/ProjectDescription/ProjectDescription.tsx
@@ -1,46 +1,99 @@
 import React from "react";
 import {
   InfoList,
-  Item,
-  PItem,
   ProjectSpecs,
   WrappedItem,
 } from "@/components/SingleProject/SingleProject.style";
 import { IProject } from "@/types/types";
 import { joinArray } from "@/services/service";
-import {AuthorLi, AuthorUL, Avatar} from "@/components/SingleProject/ProjectDescription/ProjectDescription.style";
+import { ProjBtn } from "@/components/Main/Projects/projectCard/ProjectBtn/ProjectBtn.style";
+import Authors from "@/components/SingleProject/ProjectDescription/Authors";
+import InfoItem from "@/components/SingleProject/InfoItem";
 
-function ProjectDescription({ project }: { project: IProject }) {
-  const { date, authors, motivation, tags } = project;
+function ProjectDescription({
+  project,
+  infoType,
+}: {
+  project: IProject;
+  infoType: "front" | "back" | "general";
+}) {
+  const {
+    live,
+
+    doc,
+    backEndRepo,
+    frontEndRepo,
+
+    date,
+    authors,
+    motivation,
+
+    frontendTags,
+    backendTags,
+  } = project;
+
+  const general = (
+    <>
+      <InfoItem title="motivação">
+        <ProjectSpecs>{motivation}</ProjectSpecs>
+      </InfoItem>
+      <InfoItem title="autoria">
+        <ProjectSpecs>
+          <Authors authors={authors} />
+        </ProjectSpecs>
+      </InfoItem>
+      <InfoItem title="data de início">
+        <ProjectSpecs>{date}</ProjectSpecs>
+      </InfoItem>
+    </>
+  );
+  const frontStart = (
+    <>
+      <InfoItem title="tecnologias">
+        <ProjectSpecs as="ul">
+          <WrappedItem>{joinArray(frontendTags)}</WrappedItem>
+        </ProjectSpecs>
+      </InfoItem>
+      <InfoItem title="aplicação">
+        <ProjBtn style={{ marginInlineStart: "var(--s-2)" }} href={live}>
+          {live}
+        </ProjBtn>
+      </InfoItem>
+      <InfoItem title="repositório">
+        <ProjBtn style={{ marginInlineStart: "var(--s-2)" }} href={live}>
+          {frontEndRepo}
+        </ProjBtn>
+      </InfoItem>
+    </>
+  );
+
+  const backStart = (
+    <>
+      <InfoItem title="tecnologias">
+        <ProjectSpecs as="ul">
+          <WrappedItem>{joinArray(backendTags)}</WrappedItem>
+        </ProjectSpecs>
+      </InfoItem>
+      <InfoItem title="docs">
+        <ProjBtn style={{ marginInlineStart: "var(--s-2)" }} href={live}>
+          {doc}
+        </ProjBtn>
+      </InfoItem>
+      <InfoItem title="repositório">
+        <ProjBtn style={{ marginInlineStart: "var(--s-2)" }} href={live}>
+          {backEndRepo}
+        </ProjBtn>
+      </InfoItem>
+    </>
+  );
+
   return (
     <InfoList>
-      <Item>
-        <PItem>Motivação:</PItem>
-        <ProjectSpecs>{motivation}</ProjectSpecs>
-      </Item>
-      <Item>
-        <PItem>Autoria:</PItem>
-        <ProjectSpecs>
-          <AuthorUL>
-            {authors.map((author, index) => (
-              <AuthorLi key={index}>
-                <Avatar src={author.image} alt="" />
-                <span>{author.name}</span>
-              </AuthorLi>
-            ))}
-          </AuthorUL>
-        </ProjectSpecs>
-      </Item>
-      <Item>
-        <PItem>Data de início:</PItem>
-        <ProjectSpecs>{date}</ProjectSpecs>
-      </Item>
-      <Item>
-        <PItem>Tecnologias:</PItem>
-        <ProjectSpecs as="ul">
-          <WrappedItem>{joinArray(tags)}</WrappedItem>
-        </ProjectSpecs>
-      </Item>
+      {infoType === "general" && general}
+
+      {infoType === "front" && frontStart}
+
+      {infoType === "back" && backStart}
     </InfoList>
   );
 }
