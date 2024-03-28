b3b37b8f482b59d4d7a56da112df853cac34b517
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-28 12:18:57 -0300

    Remove react-wrap-balancer from SProjSection and update styles

--- src/components/SingleProject/SingleProjTour/TourPiece/TourPiece.tsx
+++ src/components/SingleProject/SingleProjTour/TourPiece/TourPiece.tsx
@@ -3,12 +3,7 @@ import React from "react";
 import { Piece } from "@/components/SingleProject/SingleProjTour/TourPiece/TourPiece.style";
 
 function TourPiece({ children }: { children: React.ReactNode }) {
-  return (
-    <Piece>
-      {children}
-      <hr style={{ borderBlockEnd: "4px solid var(--cool-grey-20)" }} />
-    </Piece>
-  );
+  return <Piece>{children}</Piece>;
 }
 
 export default TourPiece;
