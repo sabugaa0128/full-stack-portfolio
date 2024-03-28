e5a9c0cedcde21afb70460b1e212676dec255906
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-03-28 10:33:04 -0300

    Add HeroParagraph component and revise TextHero
    Introduced a reusable HeroParagraph component for encapsulating paragraph structure. Updated the TextHero function to adopt these changes, replacing the direct content with references to the HeroParagraph component and the relevant content data.

--- src/components/Main/hero/HeroInfo/TextHero/TextHero.tsx
+++ src/components/Main/hero/HeroInfo/TextHero/TextHero.tsx
@@ -1,32 +1,24 @@
-import React from 'react';
-import {Emphasis, HeroParagraph, HeroText} from "@/components/Main/hero/HeroInfo/HeroInfo.style";
+import React from "react";
+import {
+  HeroText,
+} from "@/components/Main/hero/HeroInfo/HeroInfo.style";
+import HeroParagraph from "@/components/Main/hero/HeroParagraph";
+import { Paragraph1, Paragraph2, Paragraph3 } from "@/services/hero.data";
 
 function TextHero() {
-  return <HeroText>
-    <HeroParagraph>
-      Sou um profissional{" "}
-      <Emphasis>multidisciplinar, versátil e dinâmico</Emphasis>, com
-      graduação em arquitetura e urbanismo, passagem pelo universo de
-      startups e formação em desenvolvimento web full stack pela Kenzie
-      Academy Brasil.
-    </HeroParagraph>
-    <HeroParagraph>
-      O que mais gosto em ser dev é o fato de que você está sempre{" "}
-      <Emphasis>absorvendo coisas novas</Emphasis>, tanto através de outras pessoas
-      quanto dos seus erros ao codar. Além disso considero
-      pontos fortes em minha trajetória a{" "}
-      <Emphasis>capacidade de comunicação e a iniciativa</Emphasis>.
-    </HeroParagraph>
-
-    <HeroParagraph>
-      Desta forma, ao desenvolver aplicações web através de{" "}
-      <Emphasis>Next.js, Node.js, Typescript e CSS</Emphasis>, alio
-      habilidades comportamentais e técnicas para fazer a{" "}
-      <Emphasis>experiência do usuário</Emphasis> fluida como um{" "}
-      <Emphasis> rio de águas vivas</Emphasis> e ligeira como um{" "}
-      <Emphasis>drible do Vinícius Jr.</Emphasis>
-    </HeroParagraph>
-  </HeroText>;
+  return (
+    <HeroText>
+      <HeroParagraph>
+        <Paragraph1 />
+      </HeroParagraph>
+      <HeroParagraph>
+        <Paragraph2 />
+      </HeroParagraph>
+      <HeroParagraph>
+        <Paragraph3 />
+      </HeroParagraph>
+    </HeroText>
+  );
 }
 
 export default TextHero;
---
+++ src/components/Main/hero/HeroParagraph/HeroParagraph.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Paragraph } from "@/components/Main/hero/HeroInfo/HeroInfo.style";
+import Balancer from "react-wrap-balancer";
+import { ChildrenProps } from "@/types/types";
+
+function HeroParagraph({ children }: ChildrenProps) {
+  return (
+    <Paragraph>
+      <Balancer ratio={0}>
+          {children}
+      </Balancer>
+    </Paragraph>
+  );
+}
+
+export default HeroParagraph;
