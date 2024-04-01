a565df116cc55e397850164f257dd1726a37b8c2
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-04-01 17:44:01 -0300

    Update AContent.style layout and style details
    Made some modifications in AContent.style.ts file. Primarily, these changes are centered around adjusting the padding, margin and display properties.

--- src/components/Main/Stack/AContent/AContent.style.ts
+++ src/components/Main/Stack/AContent/AContent.style.ts
@@ -5,17 +5,15 @@ export const AContainer = styled(Accordion.Content)`
   border-block-end: 2px solid var(--cool-grey-40);
 `;
 export const ContentList = styled.ul`
-  padding-block: var(--s1);
-  padding-inline-end: var(--s-1);
-  margin-inline: var(--s3) 0;
+    padding-block: var(--s1);
+    margin-inline: var(--s3) 0;
 
-  display: grid;
-  gap: var(--s0);
+    display: grid;
+    gap: var(--s0);
 
-  @media (min-width: 900px) {
-    margin: auto;
-    width: fit-content;
-  }
+    @media (min-width: 900px) {
+        width: fit-content;
+    }
 `;
 
 export const Item = styled.li`
