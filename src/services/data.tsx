e43c3c4763c111b8f4b6533f1737ec763a98805c
sabugaa0128 <sabugaarozvelt@gmail.com>
2024-04-02 11:00:47 -0300

    Update button text, README and CV link
    Changed the text on the return button in ProjectStep.tsx to improve clarity.

--- src/services/data.tsx
+++ src/services/data.tsx
@@ -1,25 +1,10 @@
 export const github = "https://github.com/brunomoleta";
 export const cvLink =
-  "https://drive.google.com/file/d/185PjPiiXbeFa_8AJ71jofswnQvrDvMvk/view?usp=drive_link";
+  "https://drive.google.com/file/d/18kMQIoKeMAG7yPdIfxs68OmmcGc7dzW8/view?usp=drive_link";
 export const linkedin =
   "https://www.linkedin.com/in/bruno-moleta-santos-desenvolvedor-full-stack/";
 
 export const heroHeading1 = "Bruno Moleta.";
-
-export const heroParagraph1 =
-  "Sou um profissional versátil e dinâmico, com graduação em arquitetura, passagem pelo universo de startups e formação em desenvolvimento web full stack pela Kenzie Academy Brasil. ";
-
-export const heroParagraph2 =
-  "Pratico o aprendizado contínuo e, como desenvolvedor tenho o privilégio de fazer parte da evolução do mercado e da vida das pessoas " +
-  "através da tecnologia.";
-export const heroParagraph3 =
-  " Os principais ingredientes de minha carreira são: ";
-export const ingredients = "cooperação e iniciativa.";
-export const heroParagraph4 =
-  "Desta forma, ao desenvolver aplicações web através de Next.js, Node.js, Typescript e CSS, " +
-  "alio habilidades comportamentais e técnicas para criar soluções escaláveis e " +
-  "que façam a experiência do usuário";
-export const heroParagraph5 = "";
 
 export const linksList = [
   {
